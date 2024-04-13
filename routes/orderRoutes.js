6fea4212c964636f3d1a9afaf28cda68016c3b85
vamshi vangari <vamsri.nov@gmail.com>
2024-04-13 22:57:57 +0530

    add auth to products

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const orderController = require('../controllers/orderController');
 const router = express.Router();
 
-router.post('/orders', orderController.createOrder);
-router.get('/orders', orderController.getAllOrders);
-router.get('/orders/:orderId', orderController.getOrderById);
-router.patch('/orders/:orderId', orderController.updateOrder);
-router.delete('/orders/:orderId', orderController.deleteOrder);
+router.post('/orders', authMiddleware, permissionCheck('order.create'), orderController.createOrder);
+router.get('/orders', authMiddleware, permissionCheck('order.list'), orderController.getAllOrders);
+router.get('/orders/:orderId', authMiddleware, permissionCheck('order.view'), orderController.getOrderById);
+router.patch('/orders/:orderId', authMiddleware, permissionCheck('order.update'), orderController.updateOrder);
+router.delete('/orders/:orderId', authMiddleware, permissionCheck('order.delete'), orderController.deleteOrder);
 
 module.exports = router;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const productController = require('../controllers/productController');
 const router = express.Router();
 
-router.post('/products', productController.createProduct);
-router.get('/products', productController.getProducts);
-router.get('/products/:productId', productController.getProductById);
-router.patch('/products/:productId', productController.updateProduct);
-router.delete('/products/:productId', productController.deleteProduct);
+router.post('/products', authMiddleware, permissionCheck('product.create'), productController.createProduct);
+router.get('/products', authMiddleware, permissionCheck('product.list'), productController.getProducts);
+router.get('/products/:productId', authMiddleware, permissionCheck('product.view'), productController.getProductById);
+router.patch('/products/:productId', authMiddleware, permissionCheck('product.update'), productController.updateProduct);
+router.delete('/products/:productId', authMiddleware, permissionCheck('product.delete'), productController.deleteProduct);
 
 module.exports = router;
